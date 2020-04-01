0942fa70d99ebceec05a4b13ecb6c32055696ee1
ntakimoto <n.takimoto.dev@gmail.com>
2020-04-01 13:04:12 +0900

    Activity追加

--- app/src/main/java/com/uppergain/mark4/MainActivity.java
+++ app/src/main/java/com/uppergain/mark4/MainActivity.java
@@ -3,6 +3,14 @@ package com.uppergain.mark4;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * スプラッシュ画面＋TOP画面クラス<br>
+ * 基底GoF:-
+ *
+ * @author ntakimoto
+ * @version 0.0.1
+ * @since 2020-04-01
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
