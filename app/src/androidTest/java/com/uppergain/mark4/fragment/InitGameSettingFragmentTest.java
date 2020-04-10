7fc9ea77baa6c0607c20f29ed8b4855f10a20ac2
ntakimoto <n.takimoto.dev@gmail.com>
2020-04-10 17:43:04 +0900

    初期設定Fragmentテストケース追加

---
+++ app/src/androidTest/java/com/uppergain/mark4/fragment/InitGameSettingFragmentTest.java
@@ -0,0 +1,151 @@
+package com.uppergain.mark4.fragment;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class InitGameSettingFragmentTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    //初期設定画面
+    @Test
+    public void 正常系_2_1_31_ニックネーム() {
+        String actual = null;
+        String expected = "";
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void 正常系_2_1_32_初期HP入力() {
+        String actual = null;
+        String expected = "";
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void 正常系_2_1_33_登録ボタン() {
+        String actual = null;
+        String expected = "";
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void 正常系_2_1_34_確認画面に遷移すること() {
+        String actual = null;
+        String expected = "";
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void 正常系_2_1_35_プリファレンスにニックネームを保存すること() {
+        String actual = null;
+        String expected = "";
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void 正常系_2_1_36_プレファレンスに初期HPを保存すること() {
+        String actual = null;
+        String expected = "";
+        assertThat(actual, is(expected));
+    }
+
+    //入力チェック
+    @Test
+    public void 正常系_2_1_37_ニックネーム文字列() {
+        String actual = null;
+        String expected = "";
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void 正常系_2_1_38_エラーメッセージを表示すること() {
+        String actual = null;
+        String expected = "";
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void 正常系_2_1_39_数字100000_1000000() {
+        String actual = null;
+        String expected = "";
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void 異常系_2_1_40_エラーメッセージを表示すること() {
+        String actual = null;
+        String expected = "";
+        assertThat(actual, is(expected));
+    }
+
+    //ログイン確認・初期設定画面
+    @Test
+    public void 正常系_2_1_41_遷移元に戻ること() {
+        String actual = null;
+        String expected = "";
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void 異常系_2_1_42_ダイアログを表示すること() {
+        String actual = null;
+        String expected = "";
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void 正常系_2_1_43_バトル画面へ遷移すること() {
+        String actual = null;
+        String expected = "";
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void 異常系_2_1_44_ダイアログを表示すること() {
+        String actual = null;
+        String expected = "";
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void 正常系_2_1_45_FireBaseに登録済みニックネームを表示すること() {
+        String actual = null;
+        String expected = "";
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void 正常系_2_1_46_FireBaseにある現在HPを表示すること() {
+        String actual = null;
+        String expected = "";
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void 正常系_2_1_47_登録したニックネームを表示すること() {
+        String actual = null;
+        String expected = "";
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void 正常系_2_1_48_設定したHPを表示すること() {
+        String actual = null;
+        String expected = "";
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void 正常系_2_1_49_ゲームの進め方についてはYouTubeでご確認ください() {
+        String actual = null;
+        String expected = "";
+        assertThat(actual, is(expected));
+    }
+}
