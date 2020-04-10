65cf3813597148a0e5b794b0e7ca724bcca984ec
ntakimoto <n.takimoto.dev@gmail.com>
2020-04-10 14:00:30 +0900

    パーミッション関連正常系OK

--- app/src/androidTest/java/com/uppergain/mark4/TopActivityTest.java
+++ app/src/androidTest/java/com/uppergain/mark4/TopActivityTest.java
@@ -1,7 +1,9 @@
 package com.uppergain.mark4;
 
+import android.Manifest;
 import android.content.Context;
 
+import androidx.core.content.ContextCompat;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 
@@ -29,57 +31,57 @@ public class TopActivityTest {
     }
 
     @Test
-    public void 正常系_1_1_1_書き込み許諾許諾ダイアログを表示すること() {
-        String actual = "";
+    public void 正常系_1_1_1_書き込み許諾済みであること() {
+        int actual = 0;
+        int expected = ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void 異常系_1_1_2_書き込み許諾しない場合アプリキルすること() {
+        String actual = null;//アプリが起動していない
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
-    public void 異常系_1_1_2_アプリキルすること() {
-        String actual = "";
+    public void 正常系_1_1_3_読み込み許諾済みであること() {
+        int actual = 0;
+        int expected = ContextCompat.checkSelfPermission(context, Manifest.permission.READ_EXTERNAL_STORAGE);
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void 異常系_1_1_4_書き込み許諾しない場合アプリキルすること() {
+        String actual = null;//アプリが起動していない
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
-    public void 正常系_1_1_3_読み込み許諾許諾ダイアログを表示すること() {
-        String actual = "";
-        String expected = "";
+    public void 正常系_1_1_5_インターネット接続許諾済みであること() {
+        int actual = 0;
+        int expected = ContextCompat.checkSelfPermission(context, Manifest.permission.INTERNET);
         assertThat(actual, is(expected));
     }
 
     @Test
-    public void 異常系_1_1_4_アプリキルすること() {
-        String actual = "";
-        String expected = "";
-        assertThat(actual, is(expected));
-    }
-
-    @Test
-    public void 正常系_1_1_5_許諾ダイアログを表示すること() {
-        String actual = "";
-        String expected = "";
-        assertThat(actual, is(expected));
-    }
-
-    @Test
-    public void 異常系_1_1_6_NGアプリキルすること() {
-        String actual = "";
+    public void 異常系_1_1_6_インターネット接続許諾しない場合アプリキルすること() {
+        String actual = null;//アプリが起動していない
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 正常系_1_2_7_3秒表示しフェードアウトすること() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 正常系_1_2_8_トップ画面に遷移すること() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
@@ -87,56 +89,56 @@ public class TopActivityTest {
     //ログイン・Google会員登録画面
     @Test
     public void 異常系_2_1_9_ダイアログを表示すること() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 正常系_2_1_10_EditTextに入力された値を取得しFireBaseを参照する() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 正常系_2_1_11_メールアドレス文字列() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 異常系_2_1_12_エラーメッセージを表示すること() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 正常系_2_1_13_パスワード文字列() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 異常系_2_1_14_エラーメッセージを表示すること() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 正常系_2_1_15_Googleアカウントボタンを表示すること() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 正常系_2_1_16_端末に登録してあるアカウント一覧を表示すること() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
@@ -144,21 +146,21 @@ public class TopActivityTest {
     //Google会員登録
     @Test
     public void 異常系_2_1_17_ダイアログを表示すること() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 正常系_2_1_18_ダイアログが表示されFireBaseにアカウント情報が登録されること() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 正常系_2_1_19_新規登録画面メールアドレスに遷移すること() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
@@ -166,35 +168,35 @@ public class TopActivityTest {
     //メールアドレス登録画面
     @Test
     public void 正常系_2_1_20_メールアドレス文字列() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 正常系_2_1_21_パスワード文字列() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 正常系_2_1_22_確認用パスワード文字列() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 正常系_2_1_23_新規登録ボタン() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 正常系_2_1_24_ダイアログを表示すること() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
@@ -202,42 +204,42 @@ public class TopActivityTest {
     //［新規登録］ボタン押下
     @Test
     public void 正常系_2_1_25_メールアドレス文字列() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 異常系_2_1_26_エラーメッセージを表示すること() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 正常系_2_1_27_パスワード文字列() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 異常系_2_1_28_エラーメッセージを表示すること() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 正常系_2_1_29_確認用パスワード文字列() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 異常系_2_1_30_エラーメッセージを表示すること() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
@@ -245,42 +247,42 @@ public class TopActivityTest {
     //初期設定画面
     @Test
     public void 正常系_2_1_31_ニックネーム() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 正常系_2_1_32_初期HP入力() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 正常系_2_1_33_登録ボタン() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 正常系_2_1_34_確認画面に遷移すること() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 正常系_2_1_35_プリファレンスにニックネームを保存すること() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 正常系_2_1_36_プレファレンスに初期HPを保存すること() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
@@ -288,28 +290,28 @@ public class TopActivityTest {
     //入力チェック
     @Test
     public void 正常系_2_1_37_ニックネーム文字列() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 正常系_2_1_38_エラーメッセージを表示すること() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 正常系_2_1_39_数字100000_1000000() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 異常系_2_1_40_エラーメッセージを表示すること() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
@@ -317,63 +319,63 @@ public class TopActivityTest {
     //ログイン確認・初期設定画面
     @Test
     public void 正常系_2_1_41_遷移元に戻ること() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 異常系_2_1_42_ダイアログを表示すること() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 正常系_2_1_43_バトル画面へ遷移すること() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 異常系_2_1_44_ダイアログを表示すること() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 正常系_2_1_45_FireBaseに登録済みニックネームを表示すること() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 正常系_2_1_46_FireBaseにある現在HPを表示すること() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 正常系_2_1_47_登録したニックネームを表示すること() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 正常系_2_1_48_設定したHPを表示すること() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 正常系_2_1_49_ゲームの進め方についてはYouTubeでご確認ください() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
@@ -381,112 +383,112 @@ public class TopActivityTest {
     //会員登録済
     @Test
     public void 正常系_2_2_1_会員登録済GAMESTARTボタンを表示すること() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 正常系_2_2_2_一連の処理が動作すること() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 異常系_2_2_3_ダイアログを表示すること() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 正常系_2_2_4_Versionチェック() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 正常系_2_2_5_ダイアログを表示すること() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 正常系_2_2_6_ダイアログを閉じること() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 正常系_2_2_7_ストア画面へ遷移すること() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 正常系_2_2_8_運営情報の有無チェック() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 正常系_2_2_9_ダイアログを表示すること() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 正常系_2_2_10_ダイアログを閉じること() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 正常系_2_2_11_メンテナンス情報有無チェック() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 正常系_2_2_12_ダイアログを表示すること() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 正常系_2_2_13_ダイアログを閉じること() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 正常系_2_2_14_バトル画面へ遷移すること() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 異常系_2_2_15_ダイアログを表示すること() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
 
     @Test
     public void 正常系_2_2_16_バトル画面へ遷移すること() {
-        String actual = "";
+        String actual = null;
         String expected = "";
         assertThat(actual, is(expected));
     }
