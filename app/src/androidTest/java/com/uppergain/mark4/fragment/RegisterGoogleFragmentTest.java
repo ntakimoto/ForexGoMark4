b2c34d9d9aeee9e219b438fc770d2c82f26af09d
ntakimoto <n.takimoto.dev@gmail.com>
2020-04-10 17:05:55 +0900

    Google新規登録画面テストケース追加

---
+++ app/src/androidTest/java/com/uppergain/mark4/fragment/RegisterGoogleFragmentTest.java
@@ -0,0 +1,103 @@
+package com.uppergain.mark4.fragment;
+
+import android.content.Context;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class RegisterGoogleFragmentTest {
+
+    Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    //ログイン・Google会員登録画面
+    @Test
+    public void 異常系_2_1_9_ダイアログを表示すること() {
+        String actual = null;
+        String expected = "";
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void 正常系_2_1_10_EditTextに入力された値を取得しFireBaseを参照する() {
+        String actual = null;
+        String expected = "";
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void 正常系_2_1_11_メールアドレス文字列() {
+        String actual = null;
+        String expected = "";
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void 異常系_2_1_12_エラーメッセージを表示すること() {
+        String actual = null;
+        String expected = "";
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void 正常系_2_1_13_パスワード文字列() {
+        String actual = null;
+        String expected = "";
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void 異常系_2_1_14_エラーメッセージを表示すること() {
+        String actual = null;
+        String expected = "";
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void 正常系_2_1_15_Googleアカウントボタンを表示すること() {
+        String actual = null;
+        String expected = "";
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void 正常系_2_1_16_端末に登録してあるアカウント一覧を表示すること() {
+        String actual = null;
+        String expected = "";
+        assertThat(actual, is(expected));
+    }
+
+    //Google会員登録
+    @Test
+    public void 異常系_2_1_17_ダイアログを表示すること() {
+        String actual = null;
+        String expected = "";
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void 正常系_2_1_18_ダイアログが表示されFireBaseにアカウント情報が登録されること() {
+        String actual = null;
+        String expected = "";
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void 正常系_2_1_19_新規登録画面メールアドレスに遷移すること() {
+        String actual = null;
+        String expected = "";
+        assertThat(actual, is(expected));
+    }
+}
