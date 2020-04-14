3b320cf7293b82a307397cc4d931188f75baded5
ntakimoto <n.takimoto.dev@gmail.com>
2020-04-14 23:10:40 +0900

    値更新ロジック実装

--- app/src/main/java/com/uppergain/mark4/control/UserInfo.java
+++ app/src/main/java/com/uppergain/mark4/control/UserInfo.java
@@ -1,14 +1,17 @@
 package com.uppergain.mark4.control;
 
-import com.uppergain.mark4.framework.io.DataIO2;
+import com.uppergain.mark4.framework.io.DataIO;
 import com.uppergain.mark4.framework.io.FileIO;
 import com.uppergain.mark4.framework.io.PrefFile;
 
 public class UserInfo extends DataCrl {
 
+    PrefFile prefFile;
+
     public UserInfo(){
         dataIO = new PrefFile();
         fileIO = new PrefFile();
+        prefFile = new PrefFile();
     }
 
     public void writeUserInfo(){
@@ -21,7 +24,18 @@ public class UserInfo extends DataCrl {
     }
 
     @Override
-    public void setDataIO(DataIO2 dataIO) {
+    public void setDataIO(DataIO dataIO) {
         this.dataIO = dataIO;
+    }
+
+    @Override
+    public void updataData(String value) {
+        dataIO.updateData("USER_STATUS",value);
+    }
+
+    @Override
+    public String getUserStatus() {
+        String status = prefFile.getUserStatus("USER_INFO");
+        return status;
     }
 }
